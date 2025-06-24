e3bbe172874a61c8998f657958ef6f2d67e919c0
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-24 21:20:45 +0300

    feat: create TemplateWrapper component and integrate it into Home page

---
+++ src/components/template-wrapper.tsx
@@ -0,0 +1,21 @@
+import TempleteBreadcrumb from './templete-breadcrumb';
+
+export default function TemplateWrapper() {
+  return (
+    <div className="bg-gray-50 px-6">
+      <TempleteBreadcrumb />
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-2/3">
+          <div className="p-4">
+          {/* TODO: add the template details here */}
+          </div>
+        </div>
+        <div className="w-full md:w-1/3">
+          <div className="p-4">
+            {/* TODO: add the template preview here */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,10 @@
 import NewTemplateHeader from "@/components/new-template-header"
+import TemplateWrapper from "@/components/template-wrapper"
 export default function Home() {
   return (
     <>
       <NewTemplateHeader />
+      <TemplateWrapper />
     </>
   )
 }
