43179a1c9ee305ebcfdba618337842768a82907d
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-24 23:52:37 +0300

    feat: implement TempleteDetails component

--- src/components/form/templete-details.tsx
+++ src/components/form/templete-details.tsx
@@ -1,9 +1,33 @@
-import FormInputHeader from '../common/form-input-header';
+import FormInputField from '../common/form-input-field'
+import FormInputHeader from '../common/form-input-header'
+import FormMultiSelect from '../common/form-multi-select'
 
 export default function TempleteDetails() {
   return (
     <>
-      <FormInputHeader title="Template Details" description="Define your template name and language" />
+      <FormInputHeader
+        title='Template Details'
+        description='Define your template name and language'
+      />
+      <div className='my-2 p-4 bg-white rounded-sm'>
+        <FormInputField
+          label='Template Name'
+          placeholder='Template Name'
+          name='templateName'
+          onChangeHandler={() => {}}
+        />
+        <div className='mt-2'>
+        <FormMultiSelect
+          options={[
+            { key: '', value: 'Select a language' },
+            { key: 'en_US', value: 'English(US)' },
+            { key: 'ar_SA', value: 'Arabic(SA)' },
+          ]}
+          label='Language'
+          onChangeHandler={() => {}}
+        />
+        </div>
+      </div>
     </>
   )
 }
