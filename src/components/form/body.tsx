cee30bb2ba42972d4cf92835f0bbc4c310f71aa4
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-27 19:51:54 +0300

    feat: add body validation and error message handling in Body component

--- src/components/form/body.tsx
+++ src/components/form/body.tsx
@@ -1,28 +1,34 @@
-import { useTemplate } from '@/context/templateHook';
-import FormInputHeader from '../common/form-input-header';
+import { useTemplate } from '@/context/templateHook'
+import FormInputHeader from '../common/form-input-header'
 
 export default function Body() {
-  const { setBody } = useTemplate();
-  
+  const { setBody, runValidation, body } = useTemplate()
+  const bodyErrorMsg = runValidation && !body && 'Body is required'
   return (
     <div className='mt-8'>
-      <FormInputHeader title='Body' description='Enter the text of your message in the language you’ve selected' />
-      <div className='p-4 space-y-3 bg-white mt-1 rounded-sm'>
-      <textarea
-        name='body'
-        className="w-full focus:outline-none focus:ring-2 focus:ring-gray-500"
-        placeholder='Template message' 
-        style={{
-          outline: 'none',
-          border: '1px solid #C2C2C2',
-          borderRadius: '4px',
-          padding: '8px',
-          fontSize: '16px',
-        }}
-        onChange={(e) => {
-          setBody(e.target.value);
-        }}
+      <FormInputHeader
+        title='Body'
+        description='Enter the text of your message in the language you’ve selected'
       />
+      <div className='p-4 bg-white mt-1 rounded-sm'>
+        <textarea
+          name='body'
+          className='w-full h-[100px] focus:outline-none focus:ring-2 focus:ring-gray-500'
+          placeholder='Template message'
+          style={{
+            outline: 'none',
+            border: '1px solid #C2C2C2',
+            borderRadius: '4px',
+            padding: '8px',
+            fontSize: '16px',
+          }}
+          onChange={e => {
+            setBody(e.target.value)
+          }}
+        />
+        {bodyErrorMsg && (
+          <span className='text-red-500 mt-1'>{bodyErrorMsg as string}</span>
+        )}
       </div>
     </div>
   )
