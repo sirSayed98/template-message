fa3f27f3803babf76f52f1b5fec972dc5edc9a2f
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-27 19:41:56 +0300

    feat: add language error handling to TempleteDetails component

--- src/components/form/templete-details.tsx
+++ src/components/form/templete-details.tsx
@@ -1,11 +1,22 @@
-import { useTemplate } from '@/context/templateHook';
-import FormInputField from '@/components/common/form-input-field';
-import FormInputHeader from '@/components/common/form-input-header';
-import FormMultiSelect from '@/components/common/form-multi-select';
+import { useTemplate } from '@/context/templateHook'
+import FormInputField from '@/components/common/form-input-field'
+import FormInputHeader from '@/components/common/form-input-header'
+import FormMultiSelect from '@/components/common/form-multi-select'
 
 export default function TempleteDetails() {
-  const { setTemplateName, setLanguage , templateName, runValidation} = useTemplate();
-  const templateNameErrorMsg = runValidation && !templateName && 'Template name is required';
+  const {
+    setTemplateName,
+    setLanguage,
+    templateName,
+    language,
+    runValidation,
+  } = useTemplate()
+  
+  const templateNameErrorMsg =
+    runValidation && !templateName && 'Template name is required'
+  
+  const languageErrorMsg = runValidation && !language && 'Language is required'
+  
   return (
     <div className='mt-8'>
       <FormInputHeader
@@ -17,23 +28,24 @@ export default function TempleteDetails() {
           label='Template Name'
           placeholder='Template Name'
           name='templateName'
-          onChangeHandler={(e) => {
-            setTemplateName(e.target.value);
+          onChangeHandler={e => {
+            setTemplateName(e.target.value)
           }}
           error={templateNameErrorMsg as string}
         />
         <div className='mt-2'>
-        <FormMultiSelect
-          options={[
-            { key: '', value: 'Select a language' },
-            { key: 'en_US', value: 'English(US)' },
-            { key: 'ar_SA', value: 'Arabic(SA)' },
-          ]}
-          label='Language'
-          onChangeHandler={(e) => {
-            setLanguage(e);
-          }}
-        />
+          <FormMultiSelect
+            options={[
+              { key: '', value: 'Select a language' },
+              { key: 'en_US', value: 'English(US)' },
+              { key: 'ar_SA', value: 'Arabic(SA)' },
+            ]}
+            label='Language'
+            onChangeHandler={e => {
+              setLanguage(e)
+            }}
+            error={languageErrorMsg as string}
+          />
         </div>
       </div>
     </div>
