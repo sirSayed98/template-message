99983046988a5b39955bc2f16a0da67e62006c3b
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-27 20:19:42 +0300

    feat: add footer validation and error message handling in Footer component

--- src/components/form/footer.tsx
+++ src/components/form/footer.tsx
@@ -1,25 +1,29 @@
-import { useTemplate } from '@/context/templateHook';
-import FormInputField from '../common/form-input-field';
-import FormInputHeader from '../common/form-input-header';
+import { useTemplate } from '@/context/templateHook'
+import FormInputField from '../common/form-input-field'
+import FormInputHeader from '../common/form-input-header'
 
 export default function Footer() {
-  const { setFooter } = useTemplate();
+  const { setFooter, runValidation, footer } = useTemplate()
+  const footerErrorMsg = runValidation && !footer && 'Footer is required'
   return (
     <div className='mt-8'>
-     <FormInputHeader 
-      title='Footer' 
-      description='Use this section for optional details like disclaimers, contact info, or additional context.' 
-     />
-     <div className='p-4 space-y-3 bg-white mt-1 rounded-sm'>
-     <FormInputField
-      name='footer'
-      label=''
-      placeholder='Footer'
-      onChangeHandler={(e) => {
-        setFooter(e.target.value);
-      }}
-     />
-     </div>
+      <FormInputHeader
+        title='Footer'
+        description='Use this section for optional details like disclaimers, contact info, or additional context.'
+      />
+      <div className='p-4 space-y-3 bg-white mt-1 rounded-sm'>
+        <FormInputField
+          name='footer'
+          label=''
+          placeholder='Footer'
+          onChangeHandler={e => {
+            setFooter(e.target.value)
+          }}
+        />
+        {footerErrorMsg && (
+          <span className='text-red-500 mt-1'>{footerErrorMsg as string}</span>
+        )}
+      </div>
     </div>
   )
 }
