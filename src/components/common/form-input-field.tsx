6031dfdf14099872c16078c800eb48ebc57cd8fe
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-27 19:35:39 +0300

    feat: enhance FormInputField component to include error handling

--- src/components/common/form-input-field.tsx
+++ src/components/common/form-input-field.tsx
@@ -1,21 +1,36 @@
-import './form.css';
-export default function FormInputField({ label, placeholder, name, onChangeHandler }: { label: string, placeholder: string, name: string, onChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+import './form.css'
+export default function FormInputField({
+  label,
+  placeholder,
+  name,
+  onChangeHandler,
+  error,
+}: {
+  label: string
+  placeholder: string
+  name: string
+  onChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void
+  error?: string
+}) {
   return (
     <div>
-      {
-      label &&
-         <label htmlFor={name} className="block text-sm font-normal text-gray-700 mb-2">
-          {label} <span className="text-red-500">*</span>
+      {label && (
+        <label
+          htmlFor={name}
+          className='block text-sm font-normal text-gray-700 mb-2'
+        >
+          {label} <span className='text-red-500'>*</span>
         </label>
-      }
-      
+      )}
+
       <input
-        type="text"
+        type='text'
         name={name}
         onChange={onChangeHandler}
-        className="w-full bg-white px-3 py-2 border focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500"
+        className='w-full bg-white px-3 py-2 border focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500'
         placeholder={placeholder}
       />
+      <span className='text-red-500 mt-1'>{error}</span>
     </div>
   )
 }
