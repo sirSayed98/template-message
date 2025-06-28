93578a47b3b10c9b1227eaef92751dc5d7883d0c
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-28 11:52:56 +0300

    update responsiveness for template wrapper

--- src/components/template-wrapper.tsx
+++ src/components/template-wrapper.tsx
@@ -6,11 +6,11 @@ export default function TemplateWrapper() {
   return (
     <div className="bg-gray-50 px-6">
       <TempleteBreadcrumb />
-      <div className="flex flex-col md:flex-row">
-        <div className="w-full md:w-2/3">
+      <div className="flex flex-col lg:flex-row">
+        <div className="w-full lg:w-2/3">
             <FormContainer />
         </div>
-        <div className="w-full md:w-1/3">
+        <div className="w-full lg:w-1/3">
           <div className="p-4">
             <PreviewWrapper />
           </div>
