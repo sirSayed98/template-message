37dab8cef47791a17fcd42c3c0119b9cc2e0567c
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-30 21:25:32 +0300

    feat: add a new styled div to the PreviewWrapper component for enhanced visual layout

--- src/components/preview/preview-wrapper.tsx
+++ src/components/preview/preview-wrapper.tsx
@@ -1,13 +1,16 @@
-import MessagesContainer from './messages-container';
-import PhoneWrapper from './phone-wrapper';
+import MessagesContainer from './messages-container'
+import PhoneWrapper from './phone-wrapper'
 
 export default function PreviewWrapper() {
   return (
     <>
-      <h1 className="text-base mb-2 mt-5 text-center">
+      <h1 className='text-base mb-2 mt-5 text-center'>
         Preview
         <PhoneWrapper>
           <MessagesContainer />
+          <div className='absolute bottom-10 left-0 right-0 mb-2 h-3 ml-2 mr-2'>
+            <div className='bg-white h-10 rounded-3xl'></div>
+          </div>
         </PhoneWrapper>
       </h1>
     </>
