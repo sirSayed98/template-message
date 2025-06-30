6476c46a0fa280a61675b190b6386edabd7db343
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-30 16:31:46 +0300

    feat: make new template header fixed at the top of the page for improved navigation

--- src/components/new-template-header.tsx
+++ src/components/new-template-header.tsx
@@ -4,24 +4,27 @@ import { Plus, Save } from 'lucide-react'
 export default function NewTemplateMessage() {
   const { setRunValidation, getStructuredJSON } = useTemplate()
   return (
-    <div className='flex items-center justify-between border-b border-[#F1F3F5] p-2'>
-      <div className='flex items-center space-x-2 text-black text-sm font-medium'>
-        <Plus size={16} className='text-gray-700' />
-        <span className='font-medium text-2xl'>New Template Message</span>
-      </div>
+    // make this div nav fixed at the top of the page
+    <div className='mb-12'>
+      <div className='fixed top-0 left-0 right-0 z-10 bg-white flex items-center justify-between border-b border-[#F1F3F5] p-2'>
+        <div className='flex items-center space-x-2 text-black text-sm font-medium'>
+          <Plus size={16} className='text-gray-700' />
+          <span className='font-medium text-2xl'>New Template Message</span>
+        </div>
 
-      <button
-        type='submit'
-        onClick={(e) => {
-          e.preventDefault()
-          setRunValidation(true)
-          getStructuredJSON()
-        }}
-        className='flex items-center gap-2 bg-[#9b7cb7] text-white text-sm px-4 py-2 rounded cursor-pointer'
-      >
-        <Save size={16} />
-        <span className='text-sm font-medium'>Save</span>
-      </button>
+        <button
+          type='submit'
+          onClick={e => {
+            e.preventDefault()
+            setRunValidation(true)
+            getStructuredJSON()
+          }}
+          className='flex items-center gap-2 bg-[#9b7cb7] text-white text-sm px-4 py-2 rounded cursor-pointer'
+        >
+          <Save size={16} />
+          <span className='text-sm font-medium'>Save</span>
+        </button>
+      </div>
     </div>
   )
 }
