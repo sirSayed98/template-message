a3598b3bfea1c76c308f76a6b51b9d14ee3be1ff
sirSayed98 <mohamed.sayed8899@gmail.com>
2025-06-30 20:20:57 +0300

    refactor: simplify message visibility logic and enhance styling for message container

--- src/components/preview/messages-container.tsx
+++ src/components/preview/messages-container.tsx
@@ -6,19 +6,14 @@ import { Phone, SquareArrowOutUpRight } from 'lucide-react';
 export default function MessageContainer() {
   const { body, footer, header, buttons } = useTemplate();
   const currentTime = getCurrentTimeFormatted();
-  const shouldShowMessage = body.length > 0 ||
-    footer.length > 0 ||
-    (header?.format !== 'none' && (header.value?.text || header?.value?.image?.preview)) ||
-    buttons.length > 0;
-
-  if (!shouldShowMessage) return null;
+ 
   return (
     <>
     <div
       className={`transform transition-all duration-500 ease-out translate-x-0 translate-y-0 opacity-100 scale-100`}
     >
       {/* Main Bubble */}
-      <div className="text-left relative bg-white rounded-md shadow-xl border border-gray-200 p-3 max-w-sm">
+      <div className="min-h-[200px] text-left relative bg-white rounded-md shadow-xl border border-gray-200 p-3 max-w-sm">
         <div className="absolute w-4 h-3 bg-white transform rotate-50 left-[-5px] top-[12px]"></div>
         <div>
           {header.format === 'image' && header.value?.image?.preview && (
@@ -32,12 +27,12 @@ export default function MessageContainer() {
           )}
 
           {/* body */}
-          <p className="text-gray-700 text-sm">
+          <p className="text-gray-700 text-sm w-full break-words">
             {body}
           </p>
           {/* footer */}
           {footer && (
-            <div className="flex justify-between items-center mt-4 text-xs text-gray-400">
+            <div className="flex justify-between items-center mt-4 text-xs text-gray-400 break-words">
               <span>{footer}</span>
               <span>{currentTime}</span>
             </div>
@@ -47,7 +42,7 @@ export default function MessageContainer() {
           {buttons.map((button, index) => (
             <>
               <hr className="border-gray-200" />
-              <button key={index} className="w-full flex items-center justify-center gap-1 py-3 text-[#00A5F4] font-medium cursor-pointer">
+              <button key={index} className="w-full flex items-center justify-center gap-1 py-3 text-[#00A5F4] font-medium cursor-pointer break-words">
                 {button.type === 'URL' && <SquareArrowOutUpRight size={16} />}
                 {button.type === 'CALL' && <Phone size={16} />}
                 {button.text}
